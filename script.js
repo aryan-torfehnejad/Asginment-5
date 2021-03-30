465a28b936505691dce2240e214bb4a0e3abc62c
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-30 17:19:34 +0000

    fixed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let number = 0
 let area = 0
 let circumference = 0
-let Pi=3.14159265
+const Pi = 3.14159265
 
-document.getElementById('button').addEventListener('click',click)
+document.getElementById('button').addEventListener('click', click)
 function click () {
- number = document.getElementById('input').value
-number = parseInt(number)
-if (isNaN(number)|| (isNaN(area)) || (isNaN(circumference))) output.textContent='you did not enter the diameter'
- area = number /2 ** 2 * Pi
- circumference = number/2 * Pi * 2
-  output.textContent = "Area =" + area
-    output2.textContent = "Circumference =" + circumference
+  number = document.getElementById('input').value
+  number = parseInt(number)
+  if (isNaN(number) || (isNaN(area)) || (isNaN(circumference))) output.textContent = 'you did not enter the diameter'
+  area = number / 2 ** 2 * Pi
+  circumference = number / 2 * Pi * 2
+  output.textContent = 'Area =' + area
+  output2.textContent = 'Circumference =' + circumference
 }
